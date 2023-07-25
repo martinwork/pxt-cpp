52e8b9cef8078829caa08ec1128d8fccb881888c
Martin Williams <martinwork@users.noreply.github.com>
2023-07-25 19:18:09 +0100

    Add more macros

--- ext.cpp
+++ ext.cpp
@@ -31,13 +31,29 @@ namespace cpptest {
 //% block="one"
 //%
 void one() {
-#if MICROBIT_CODAL
-    char macro1[] = "MICROBIT_BLE_UTILITY_SERVICE_PAIRING = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE_PAIRING) "\n";
-    char macro2[] = "MICROBIT_BLE_UTILITY_SERVICE         = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE) "\n";
+    char macro1[] = "DEVICE_BLE                               = " MACROTOSTRING(DEVICE_BLE) "\n";
+    char macro2[] = "MICROBIT_BLE_ENABLED                     = " MACROTOSTRING(MICROBIT_BLE_ENABLED) "\n";
+    char macro3[] = "MICROBIT_BLE_PAIRING_MODE                = " MACROTOSTRING(MICROBIT_BLE_PAIRING_MODE) "\n";
+    char macro4[] = "MICROBIT_BLE_DFU_SERVICE                 = " MACROTOSTRING(MICROBIT_BLE_DFU_SERVICE) "\n";
+    char macro5[] = "MICROBIT_BLE_DEVICE_INFORMATION_SERVICE  = " MACROTOSTRING(MICROBIT_BLE_DEVICE_INFORMATION_SERVICE) "\n";
+    char macro6[] = "MICROBIT_BLE_EVENT_SERVICE               = " MACROTOSTRING(MICROBIT_BLE_EVENT_SERVICE) "\n";
+    char macro7[] = "MICROBIT_BLE_PARTIAL_FLASHING            = " MACROTOSTRING(MICROBIT_BLE_PARTIAL_FLASHING) "\n";
+    char macro8[] = "MICROBIT_BLE_SECURITY_LEVEL              = " MACROTOSTRING(MICROBIT_BLE_SECURITY_LEVEL) "\n";
+    char macro9[] = "MICROBIT_BLE_SECURITY_MODE               = " MACROTOSTRING(MICROBIT_BLE_SECURITY_MODE) "\n";
+    char macroA[] = "MICROBIT_BLE_UTILITY_SERVICE_PAIRING     = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE_PAIRING) "\n";
+    char macroB[] = "MICROBIT_BLE_UTILITY_SERVICE             = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE) "\n";
 
     uBit.serial.send( macro1);
     uBit.serial.send( macro2);
-#endif // MICROBIT_CODAL
+    uBit.serial.send( macro3);
+    uBit.serial.send( macro4);
+    uBit.serial.send( macro5);
+    uBit.serial.send( macro6);
+    uBit.serial.send( macro7);
+    uBit.serial.send( macro8);
+    uBit.serial.send( macro9);
+    uBit.serial.send( macroA);
+    uBit.serial.send( macroB);
 } 
 
 }
